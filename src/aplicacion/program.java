23a86c59f65da17bbb7f909676627f937d6ff3da
joao batista <joaobatysta1975@gmail.com>
2020-08-31 10:27:27 -0300

    acrescentada mensagem de bom dia

--- src/aplicacion/program.java
+++ src/aplicacion/program.java
@@ -4,7 +4,10 @@ public class program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		 System.out.println ("boa noite");
+		System.out.println("bom dia");
 	}
-
+	
+	
 }
